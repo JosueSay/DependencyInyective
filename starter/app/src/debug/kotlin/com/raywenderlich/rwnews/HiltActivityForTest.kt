486044d56e38bc52bb18edcb64da0da41cab9169
JosueSay <106031855+JosueSay@users.noreply.github.com>
2023-10-13 19:59:38 -0600

    ---

--- starter/app/src/debug/kotlin/com/raywenderlich/rwnews/HiltActivityForTest.kt
+++
@@ -1,6 +0,0 @@
-package com.raywenderlich.rwnews
-import androidx.appcompat.app.AppCompatActivity
-import dagger.hilt.android.AndroidEntryPoint
-
-@AndroidEntryPoint // HERE
-class HiltActivityForTest : AppCompatActivity()
